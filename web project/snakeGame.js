0bc9064903571481869ed031b4bf836d020bc9ca
psyeh7 <psyeh7@exmail.nottingham.ac.uk>
2024-06-26 23:05:55 +0100

    Added movement to snake, but speed needs to be adjusted

--- web project/snakeGame.js
+++ web project/snakeGame.js
@@ -18,6 +18,9 @@ var context; //what we use to draw with
 //snake code!
 var snakeX = blockSize * 5;
 var snakeY = blockSize * 5; // Will begin at 5,5. 
+var velocityX = 0;//Snake needs to have a speed because it is moving
+var velocityY = 0; //begin at zero as it begins the game still
+
 
 //apple code!
 var appleX;
@@ -30,16 +33,21 @@ window.onload = function(){ //this happens when window is loaded.
     board.width = cols * blockSize;
     context = board.getContext("2d");
 
-    randomFood();
-    update(); //this function will redraw the board
+    randomFood(); //places!
+    document.addEventListener("keyup", changeDirection);
+  
+    setInterval(update, 1000/10); //every hundred milliseconds, it will run the update function
 }
 
 function update(){
+    //board (canvas)
     context.fillStyle = "lightGreen";
     context.fillRect(0,0,board.width,board.height) //starting at the corner of the page e.g (0,0) and make it light green
 
     //snake
     context.fillStyle="darkBlue";
+    snakeX += velocityX;
+    snakeY += velocityY;
     context.fillRect(snakeX,snakeY,blockSize,blockSize); // paints at the cooardinate of 5,5 (snakex/y) and fills one square 
 
     //Apple
@@ -48,7 +56,32 @@ function update(){
 }
 
 function randomFood(){ //This ensure the apple isn't fixed in one place!
-    appleX = Math.floor(Math.random() * cols) * blockSize;
+    appleX = Math.floor(Math.random() * cols) * blockSize; //(0-19) * 25 gives us our column number!
     appleY = Math.floor(Math.random() * rows) * blockSize;
+}
+
+function changeDirection(e){
+    if (e.code == "ArrowUp"){ //Going up!
+        velocityX = 0;
+        velocityY = -1;
+
+    }
+
+    else if (e.code == "ArrowDown"){ //Going up!
+        velocityX = 0;
+        velocityY = 1;
+
+    }
+
+    else if (e.code == "ArrowLeft"){ //Going up!
+        velocityX = -1;
+        velocityY = 0;
+    }
+
+    else if (e.code == "ArrowRight"){ //Going up!
+        velocityX = 1
+        velocityY = 0;
+
+    }
 }
 
