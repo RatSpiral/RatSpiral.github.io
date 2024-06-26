3968cb5b55848e5715b9b05b3fb64f9f0157e645
psyeh7 <psyeh7@exmail.nottingham.ac.uk>
2024-06-27 00:01:17 +0100

    Body increase after eating apple!

--- web project/snakeGame.js
+++ web project/snakeGame.js
@@ -20,7 +20,7 @@ var snakeX = blockSize * 5;
 var snakeY = blockSize * 5; // Will begin at 5,5. 
 var velocityX = 0;//Snake needs to have a speed because it is moving
 var velocityY = 0; //begin at zero as it begins the game still
-
+var snakeBody = [];
 
 //apple code!
 var appleX;
@@ -41,24 +41,47 @@ window.onload = function(){ //this happens when window is loaded.
 
 function update(){
     //board (canvas)
-    context.fillStyle = "lightGreen";
-    context.fillRect(0,0,board.width,board.height) //starting at the corner of the page e.g (0,0) and make it light green
+    drawBoard();
+    //context.fillStyle = "lightGreen";
+    //context.fillRect(0,0,board.width,board.height) //starting at the corner of the page e.g (0,0) and make it light green
 
     //Apple
     context.fillStyle = "red";
     context.fillRect(appleX,appleY,blockSize,blockSize);
 
     if (snakeX == appleX && snakeY == appleY){
+        snakeBody.push([appleX,appleY]); //grows the next part of the body where the food was. 
         randomFood();
     }
 
+    for(let i = snakeBody.length-1; i>0; i--){
+        snakeBody[i] = snakeBody[i-1];
+    }
+
+    if(snakeBody.length){
+        snakeBody[0] = [snakeX,snakeY]
+    }
+
     //snake
+    createSnake();
+   
+
+    
+}
+
+
+//!!!!!!I COULD ADD POISON WHICH RANDOMLY GENERATES
+ 
+function createSnake(){
     context.fillStyle="darkBlue";
     snakeX += velocityX * blockSize/2;
     snakeY += velocityY * blockSize/2;
     context.fillRect(snakeX,snakeY,blockSize,blockSize); // paints at the cooardinate of 5,5 (snakex/y) and fills one square 
 
-    
+    for (let i = 0; i <snakeBody.length;i++){
+        context.fillRect(snakeBody[i][0],snakeBody[i][1],blockSize,blockSize);
+    }
+
 }
 
 function randomFood(){ //This ensure the apple isn't fixed in one place!
@@ -67,27 +90,46 @@ function randomFood(){ //This ensure the apple isn't fixed in one place!
 }
 
 function changeDirection(e){
-    if (e.code == "ArrowUp"){ //Going up!
+    if (e.code == "ArrowUp" && velocityY != 1){ //Going up! Can't eat own tail, so additional check added for all directions
         velocityX = 0;
         velocityY = -1;
 
     }
 
-    else if (e.code == "ArrowDown"){ //Going up!
+    else if (e.code == "ArrowDown" && velocityY != -1){ //Going up!
         velocityX = 0;
         velocityY = 1;
 
     }
 
-    else if (e.code == "ArrowLeft"){ //Going up!
+    else if (e.code == "ArrowLeft" && velocityX != 1){ //Going up!
         velocityX = -1;
         velocityY = 0;
     }
 
-    else if (e.code == "ArrowRight"){ //Going up!
+    else if (e.code == "ArrowRight" && velocityX != -1){ //Going up!
         velocityX = 1
         velocityY = 0;
 
     }
 }
+
+function drawBoard(){
+    for (let rowCount = 0; rowCount < rows; rowCount++) {
+        for (let colCount = 0; colCount < cols; colCount++) {
+            if ((rowCount + colCount) % 2 == 0) { //Creating an alternating grid of lightGreen and paleGreens
+                context.fillStyle = 'lightGreen';
+            } else {
+                context.fillStyle = 'paleGreen';
+            }
+            // Draw the square
+            context.fillRect(colCount * blockSize, rowCount * blockSize, blockSize, blockSize);
+        }
+    }
+}
+
+
+
+
+
 
