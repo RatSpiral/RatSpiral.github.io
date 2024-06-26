6bbf4fd07be4e04af37c3b1c147772915dc59b51
psyeh7 <psyeh7@exmail.nottingham.ac.uk>
2024-06-26 22:51:58 +0100

    Apple is drawn onto canvas, function controls its placement

--- web project/snakeGame.js
+++ web project/snakeGame.js
@@ -15,6 +15,14 @@ var cols = 20;
 var board;
 var context; //what we use to draw with
 
+//snake code!
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5; // Will begin at 5,5. 
+
+//apple code!
+var appleX;
+var appleY;
+
 window.onload = function(){ //this happens when window is loaded.
     //get the board
     board = document.getElementById("board");
@@ -22,11 +30,25 @@ window.onload = function(){ //this happens when window is loaded.
     board.width = cols * blockSize;
     context = board.getContext("2d");
 
-
+    randomFood();
     update(); //this function will redraw the board
 }
 
 function update(){
     context.fillStyle = "lightGreen";
     context.fillRect(0,0,board.width,board.height) //starting at the corner of the page e.g (0,0) and make it light green
+
+    //snake
+    context.fillStyle="darkBlue";
+    context.fillRect(snakeX,snakeY,blockSize,blockSize); // paints at the cooardinate of 5,5 (snakex/y) and fills one square 
+
+    //Apple
+    context.fillStyle = "red";
+    context.fillRect(appleX,appleY,blockSize,blockSize);
 }
+
+function randomFood(){ //This ensure the apple isn't fixed in one place!
+    appleX = Math.floor(Math.random() * cols) * blockSize;
+    appleY = Math.floor(Math.random() * rows) * blockSize;
+}
+
